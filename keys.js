f478803fa8402ae0e61beea0ec6bddde8b291f13
Syed Khurshid <magnite@SyedKhushidsMBP.lan>
2017-03-30 00:03:26 -0500

    Added keys.js file

---
+++ keys.js
@@ -0,0 +1,10 @@
+exports.twitterKeys = {
+  consumer_key: 'Some Key',
+  consumer_secret: 'Some Key',
+  access_token_key: 'Some Key',
+  access_token_secret: 'Some Key',
+}
+
+exports.omdb = {
+  lock: "Some Key",
+}
